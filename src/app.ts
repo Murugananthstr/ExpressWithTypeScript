4076e79a35c7a8a2d9f0af33488ab8709a6d29c3
Ananth <ananthsoft@gmail.com>
2023-12-13 20:00:57 -0800

    Init express

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import { ok } from "assert";
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app
+  .route("/api/books")
+  .get((req: Request, res: Response) => {
+    return res.send("You made a get request");
+  })
+  .post((req: Request, res: Response) => {
+    return res.send("You made a POST request");
+  })
+  .put((req: Request, res: Response) => {
+    return res.send("You made a PUT request");
+  })
+  .all((req: Request, res: Response) => {
+    return res.send("You made an X request");
+  });
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({ test1: "test2" });
+});
+
+app.post("/api/data", (req: Request, res: Response) => {
+  console.log(req.body);
+  return res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log("Application listening at http://localhost:3000");
+});
